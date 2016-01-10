cd5b3c0bd386fe9677921333b29486272a68576e
Yongzheng Huang <iamhuangyz@gmail.com>
2016-01-09 20:53:27 -0600

    nothing

--- index.js
+++ index.js
@@ -17,6 +17,4 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-console.log("!!!!!!!!!!!!!!");
-console.log("!!!!!!!!!!!!!!");
-console.log("!!!!!!!!!!!!!!");
+console.log("hello world");
